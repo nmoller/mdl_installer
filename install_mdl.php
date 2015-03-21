2920ad84797145272d75f6a26fbea876e6671c38
Nelson Moller <nmoller@cegepadistance.ca>
2015-03-20 21:21:19 -0400

    Je m'étais trompé dans le constructor...

--- install_mdl.php
+++ install_mdl.php
@@ -4,28 +4,30 @@ class installer{
   public $root;
   public $debug = false;
 
-  public function __contruct(){
-    
+  public function __construct($root = 'mdl', $follow = 'MOODLE_28_STABLE'){
+    $this->follows = $follow;
+    $this->root = __DIR__.'/'.$root;
+
   }
 
   public function add_repo($origin, $path = ''){
-    $this->repositories[__DIR__.'/mdl/'.$path] = $origin;
+    $this->repositories[$this->root.'/'.$path] = $origin;
   }
 
-  public function debug($debug = false){
-    $this->debug = $debug;
+  public function debug(){
+    $this->debug = true;
   }
 
   public function exec(){
     foreach ($this->repositories as $path => $origin){
-      if (!file_exists($path) && $path !== __DIR__.'/mdl/') {
+      if (!file_exists($path) && $path !== $this->root) {
          $this->message("Clonning");
          $clone = $this->git('clone');  
          $clone($origin.' '.$path);    
       }
       else if (!file_exists($path)){
           $clone = $this->git('clone');  
-          $clone('-b MOODLE_28_STABLE '.$origin.' '.$path);
+          $clone('-b '.$this->follows.' '.$origin.' '.$path);
           $this->message("MDL-created");
           /*
          shell_exec('cd '.$path);
@@ -67,7 +69,7 @@ class installer{
 }
 
 $test = new installer();
-$test->debug(true);
+$test->debug();
 $test->add_repo('https://github.com/moodle/moodle.git');
 $test->add_repo('https://github.com/nmoller/local_scripts.git','local/scripts');
 $test->exec();
