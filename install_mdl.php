00ed1db997534a8aea88b96253cc2ec8c9607668
Nelson Moller <nmoller@cegepadistance.ca>
2015-03-20 20:48:34 -0400

    Ajout d'option pour afficher des messages lors du debug.

--- install_mdl.php
+++ install_mdl.php
@@ -2,6 +2,7 @@
 class installer{
   public $repositories = array();
   public $root;
+  public $debug = false;
 
   public function __contruct(){
     
@@ -11,15 +12,21 @@ class installer{
     $this->repositories[__DIR__.'/mdl/'.$path] = $origin;
   }
 
+  public function debug($debug = false){
+    $this->debug = $debug;
+  }
+
   public function exec(){
     foreach ($this->repositories as $path => $origin){
       if (!file_exists($path) && $path !== __DIR__.'/mdl/') {
+         $this->message("Clonning");
          $clone = $this->git('clone');  
          $clone($origin.' '.$path);    
       }
       else if (!file_exists($path)){
           $clone = $this->git('clone');  
           $clone('-b MOODLE_28_STABLE '.$origin.' '.$path);
+          $this->message("MDL-created");
           /*
          shell_exec('cd '.$path);
          $remote = $this->git('remote');
@@ -32,11 +39,13 @@ class installer{
       }
       else {
          shell_exec('cd '.$path);
+          $this->message($path);
          $fetch = $this->git('fetch');
          $fetch('origin');
 
          $merge = $this->git('merge');
          $merge('origin/master');
+         $this->message("Updating");
       }
       
     }
@@ -50,10 +59,15 @@ class installer{
     };
   }
 
+  public function message($text){
+    if ($this->debug) echo $text.PHP_EOL;
+  }
+
 
 }
 
 $test = new installer();
+$test->debug(true);
 $test->add_repo('https://github.com/moodle/moodle.git');
 $test->add_repo('https://github.com/nmoller/local_scripts.git','local/scripts');
 $test->exec();
